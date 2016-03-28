2441a197143d635ee8420c9b38a31338a6ff151b
Ginger <cmollgaard@gmail.com>
2016-03-28 17:34:36 +0200

    der

--- Assignment2/nlpdat/run.sh
+++ Assignment2/nlpdat/run.sh
@@ -9,19 +9,19 @@ perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/traindat0.tok' > '../nlpdat/trainda
 java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -prop ../nlpdat/ner0.prop
 java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ../nlpdat/ner-model0.ser.gz -testFile ../nlpdat/traindat0.tok.tsv > ../nlpdat/res0
 
-#fold 1
-java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/testdat1.tsv' > '../nlpdat/testdat1.tok'
-perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/testdat1.tok' > '../nlpdat/testdat1.tok.tsv'
-java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/traindat1.tsv' > '../nlpdat/traindat1.tok'
-perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/traindat1.tok' > '../nlpdat/traindat1.tok.tsv'
-java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -prop ../nlpdat/ner1.prop
-java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ../nlpdat/ner-model1.ser.gz -testFile ../nlpdat/traindat1.tok.tsv> ../nlpdat/res1
-
-
-#fold 2
-java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/testdat2.tsv' > '../nlpdat/testdat2.tok'
-perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/testdat2.tok' > '../nlpdat/testdat2.tok.tsv'
-java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/traindat2.tsv' > '../nlpdat/traindat2.tok'
-perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/traindat2.tok' > '../nlpdat/traindat2.tok.tsv'
-java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -prop ../nlpdat/ner2.prop
-java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ../nlpdat/ner-model2.ser.gz -testFile ../nlpdat/traindat2.tok.tsv> ../nlpdat/res2
+##fold 1
+#java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/testdat1.tsv' > '../nlpdat/testdat1.tok'
+#perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/testdat1.tok' > '../nlpdat/testdat1.tok.tsv'
+#java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/traindat1.tsv' > '../nlpdat/traindat1.tok'
+#perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/traindat1.tok' > '../nlpdat/traindat1.tok.tsv'
+#java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -prop ../nlpdat/ner1.prop
+#java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ../nlpdat/ner-model1.ser.gz -testFile ../nlpdat/traindat1.tok.tsv> ../nlpdat/res1
+#
+#
+##fold 2
+#java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/testdat2.tsv' > '../nlpdat/testdat2.tok'
+#perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/testdat2.tok' > '../nlpdat/testdat2.tok.tsv'
+#java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/traindat2.tsv' > '../nlpdat/traindat2.tok'
+#perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/traindat2.tok' > '../nlpdat/traindat2.tok.tsv'
+#java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -prop ../nlpdat/ner2.prop
+#java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ../nlpdat/ner-model2.ser.gz -testFile ../nlpdat/traindat2.tok.tsv> ../nlpdat/res2
