ea51872cfe55c33a955b2a1c410d655a6b609557
Ginger <cmollgaard@gmail.com>
2016-03-28 17:10:41 +0200

    training

---
+++ Assignment2/nlpdat/run.sh
@@ -0,0 +1,27 @@
+#Needs to be run from the root of the nlp thingy. Put the whole folder next to the nlpdat folder
+
+
+#fold 0
+java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/testdat0.tsv' > '../nlpdat/testdat0.tok'
+perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/testdat0.tok' > '../nlpdat/testdat0.tok.tsv'
+java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/traindat0.tsv' > '../nlpdat/traindat0.tok'
+perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/traindat0.tok' > '../nlpdat/traindat0.tok.tsv'
+java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -prop ner0.prop
+java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ner-model0.ser.gz -testFile ../nlpdat/traindat0.tok.tsv
+
+#fold 1
+java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/testdat1.tsv' > '../nlpdat/testdat1.tok'
+perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/testdat1.tok' > '../nlpdat/testdat1.tok.tsv'
+java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/traindat1.tsv' > '../nlpdat/traindat1.tok'
+perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/traindat1.tok' > '../nlpdat/traindat1.tok.tsv'
+java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -prop ner1.prop
+java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ner-model1.ser.gz -testFile ../nlpdat/traindat1.tok.tsv
+
+
+#fold 2
+java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/testdat2.tsv' > '../nlpdat/testdat2.tok'
+perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/testdat2.tok' > '../nlpdat/testdat2.tok.tsv'
+java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/traindat2.tsv' > '../nlpdat/traindat2.tok'
+perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/traindat2.tok' > '../nlpdat/traindat2.tok.tsv'
+java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -prop ner2.prop
+java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ner-model2.ser.gz -testFile ../nlpdat/traindat2.tok.tsv
