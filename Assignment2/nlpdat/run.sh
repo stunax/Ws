0f2a2427183a20d426cc7f089227bcac744ed6fb
Ginger <cmollgaard@gmail.com>
2016-03-28 17:47:07 +0200

    asdf

--- Assignment2/nlpdat/run.sh
+++ Assignment2/nlpdat/run.sh
@@ -7,7 +7,7 @@ perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/testdat0.tok' > '../nlpdat/testdat0
 java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/traindat0.tsv' > '../nlpdat/traindat0.tok'
 perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/traindat0.tok' > '../nlpdat/traindat0.tok.tsv'
 java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -prop ../nlpdat/ner0.prop
-java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ../nlpdat/ner-model0.ser.gz -testFile ../nlpdat/traindat0.tok.tsv > ../nlpdat/res0
+java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ../nlpdat/ner-model0.ser.gz -testFile ../nlpdat/traindat0.tok.tsv > ../nlpdat/res0.txt
 
 ##fold 1
 #java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/testdat1.tsv' > '../nlpdat/testdat1.tok'
@@ -15,7 +15,7 @@ java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ../nlpdat/ner
 #java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/traindat1.tsv' > '../nlpdat/traindat1.tok'
 #perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/traindat1.tok' > '../nlpdat/traindat1.tok.tsv'
 #java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -prop ../nlpdat/ner1.prop
-#java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ../nlpdat/ner-model1.ser.gz -testFile ../nlpdat/traindat1.tok.tsv> ../nlpdat/res1
+#java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ../nlpdat/ner-model1.ser.gz -testFile ../nlpdat/traindat1.tok.tsv> ../nlpdat/res1.txt
 #
 #
 ##fold 2
@@ -24,4 +24,4 @@ java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ../nlpdat/ner
 #java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/traindat2.tsv' > '../nlpdat/traindat2.tok'
 #perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/traindat2.tok' > '../nlpdat/traindat2.tok.tsv'
 #java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -prop ../nlpdat/ner2.prop
-#java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ../nlpdat/ner-model2.ser.gz -testFile ../nlpdat/traindat2.tok.tsv> ../nlpdat/res2
+#java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ../nlpdat/ner-model2.ser.gz -testFile ../nlpdat/traindat2.tok.tsv> ../nlpdat/res2.txt
