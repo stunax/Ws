cc6e43ee6c2db149f6e3dee258c29f71085cf759
Ginger <cmollgaard@gmail.com>
2016-03-28 17:12:29 +0200

    daf

--- Assignment2/nlpdat/run.sh
+++ Assignment2/nlpdat/run.sh
@@ -6,7 +6,7 @@ java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/testdat0.tsv' > '.
 perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/testdat0.tok' > '../nlpdat/testdat0.tok.tsv'
 java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/traindat0.tsv' > '../nlpdat/traindat0.tok'
 perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/traindat0.tok' > '../nlpdat/traindat0.tok.tsv'
-java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -prop ner0.prop
+java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -prop ../nlpdat/ner0.prop
 java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ner-model0.ser.gz -testFile ../nlpdat/traindat0.tok.tsv
 
 #fold 1
@@ -14,7 +14,7 @@ java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/testdat1.tsv' > '.
 perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/testdat1.tok' > '../nlpdat/testdat1.tok.tsv'
 java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/traindat1.tsv' > '../nlpdat/traindat1.tok'
 perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/traindat1.tok' > '../nlpdat/traindat1.tok.tsv'
-java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -prop ner1.prop
+java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -prop ../nlpdat/ner1.prop
 java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ner-model1.ser.gz -testFile ../nlpdat/traindat1.tok.tsv
 
 
@@ -23,5 +23,5 @@ java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/testdat2.tsv' > '.
 perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/testdat2.tok' > '../nlpdat/testdat2.tok.tsv'
 java -cp "*" edu.stanford.nlp.process.PTBTokenizer '../nlpdat/traindat2.tsv' > '../nlpdat/traindat2.tok'
 perl -ne 'chomp; print "$_\tO\n"' '../nlpdat/traindat2.tok' > '../nlpdat/traindat2.tok.tsv'
-java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -prop ner2.prop
+java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -prop ../nlpdat/ner2.prop
 java -cp "*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ner-model2.ser.gz -testFile ../nlpdat/traindat2.tok.tsv
