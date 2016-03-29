59f1d5d6e1caae09e0fc9cf598f1b7bc687a075e
Ginger <cmollgaard@gmail.com>
2016-03-29 10:49:15 +0200

    daf

--- Assignment2/nlpdat/run.sh
+++ Assignment2/nlpdat/run.sh
@@ -2,13 +2,15 @@
 
 
 #fold 0
-java -cp "../stanford-corenlp-full-2015-12-09/*" edu.stanford.nlp.process.PTBTokenizer 'testdat0.tsv' > 'testdat0.tok'
+#java -cp "../stanford-corenlp-full-2015-12-09/*" edu.stanford.nlp.process.PTBTokenizer 'testdat0.tsv' > 'testdat0.tok'
+java -cp "../stanford-corenlp-full-2015-12-09/*" edu.stanford.nlp.sentiment.BuildBinarizedDataset 'testdat0.tsv' > 'testdat0.tok'
 #perl -ne 'chomp; print "$_\tO\n"' 'testdat0.tok' > 'testdat0.tok.tsv'
-java -cp "../stanford-corenlp-full-2015-12-09/*" edu.stanford.nlp.process.PTBTokenizer 'traindat0.tsv' > 'traindat0.tok'
+#java -cp "../stanford-corenlp-full-2015-12-09/*" edu.stanford.nlp.process.PTBTokenizer 'traindat0.tsv' > 'traindat0.tok'
+java -cp "../stanford-corenlp-full-2015-12-09/*" edu.stanford.nlp.sentiment.BuildBinarizedDataset 'traindat0.tsv' > 'traindat0.tok'
 #perl -ne 'chomp; print "$_\tO\n"' 'traindat0.tok' > 'traindat0.tok.tsv'
-java -cp "../stanford-corenlp-full-2015-12-09/*" edu.stanford.nlp.ie.crf.CRFClassifier -prop ner0.prop
-java -cp "../stanford-corenlp-full-2015-12-09/*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ner-model0.ser.gz -testFile testdat0.tok.tsv > res0.txt
-
+#java -cp "../stanford-corenlp-full-2015-12-09/*" edu.stanford.nlp.ie.crf.CRFClassifier -prop ner0.prop
+#java -cp "../stanford-corenlp-full-2015-12-09/*" edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ner-model0.ser.gz -testFile testdat0.tok.tsv > res0.txt
+java -cp "../stanford-corenlp-full-2015-12-09/*" -mx8g edu.stanford.nlp.sentiment.SentimentTraining -numHid 25 -trainPath 'traindat0.tok' -devPath 'testdat0.tok' -train -model model.ser.gz
 ##fold 1
 #java -cp "../stanford-corenlp-full-2015-12-09/*" edu.stanford.nlp.process.PTBTokenizer 'testdat1.tsv' > 'testdat1.tok'
 #perl -ne 'chomp; print "$_\tO\n"' 'testdat1.tok' > 'testdat1.tok.tsv'
